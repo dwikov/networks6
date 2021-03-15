c1e528cf410e640eccd968f3101ff5cccf5bf2df
dwikov <dwik.syria@gmail.com>
2021-03-15 17:03:26 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,58 +1,67 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <sys/socket.h>
 #include <stdlib.h>
-#include <netinet/in.h>
+#include <unistd.h>
 #include <string.h>
-#define PORT 8080
-int main(int argc, char const *argv[])
-{
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "Hello from server";
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT     8080
+#define MAXLINE 1024
+
+// Driver code
+int main() {
+    int sockfd;
+    char buffer1[MAXLINE], buffer2[MAXLINE];
+    struct sockaddr_in servaddr, cliaddr,cliaddr1,cliaddr2;
 
     // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                   &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address,
-             sizeof(address))<0)
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr1, 0, sizeof(cliaddr1));
+    memset(&cliaddr2, 0, sizeof(cliaddr2));
+    // Filling server information
+    servaddr.sin_family    = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+    cliaddr2.sin_family = AF_INET;
+    cliaddr2.sin_addr.s_addr = INADDR_ANY;
+    cliaddr2.sin_port = htons(8080);
+    cliaddr1.sin_family = AF_INET;
+    cliaddr1.sin_addr.s_addr = INADDR_ANY;
+    cliaddr1.sin_port = htons(8080);
+    // Bind the socket with the server address
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr,
+            sizeof(servaddr)) < 0 )
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                             (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+
+    int len,len1, len2, n1,n2;
+    len = sizeof(cliaddr);
+    len1 = sizeof(cliaddr1);
+    len2 = sizeof(cliaddr2);
+    n1 = recvfrom(sockfd, (char *)buffer1, MAXLINE,
+                  MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+                  &len);
+    buffer1[n1] = '\0';
+    printf("%s",buffer1);
+    sendto(sockfd, (const char *)buffer1, strlen(buffer1),
+           MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+           len);
+    
+    sendto(sockfd, (const char *)buffer1, strlen(buffer1),
+           MSG_CONFIRM, (const struct sockaddr *) &cliaddr2, len2);
+
+    printf("msg sent.\n");
+
+
+
     return 0;
 }
